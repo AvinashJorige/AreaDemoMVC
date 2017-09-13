c48a7977411b20df9f42e958e58f801357ec9a8d
Avinash_J <avinash.j@jarustech.com>
2017-09-13 22:42:14 +0530

    Adding the initial changes and empty solution for understanding the Area in the MVC

---
+++ AreaDemoMVC/Areas/Kids/KidsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AreaDemoMVC.Areas.Kids
+{
+    public class KidsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Kids";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Kids_default",
+                "Kids/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ AreaDemoMVC/Areas/Women/WomenAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace AreaDemoMVC.Areas.Women
+{
+    public class WomenAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Women";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Women_default",
+                "Women/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
